7654543737947c15bd487b24b54a03d33d0e0fce
AlexandrKomisar <monolitalex@gmail.com>
2022-10-12 17:11:39 +0300

    add drop-menu

--- js/event.js
+++ js/event.js
@@ -1,10 +1,33 @@
 let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", clicked, Option);
+
+const options = {
+  //фаза на якій повинен спрацювати обработчик
+  'capture': true,
+  //якщо true то обработчик видалиться після виконання
+  'once': false,
+  //
+  'passive': false,
+};
+
+btn.addEventListener("click", clicked, options);
 
 function clicked() {
   let newDiv = document.createElement("div");
   newDiv.className = "my-class";
-  newDiv.innerHTML = "ok";
-  console.log("ok");
+  newDiv.innerHTML = "<div></div>";
+  console.log("click");
+}
+
+// drop menu
+const menuBody = document.querySelector('.menu')
+document.addEventListener('click',menu)
+
+function menu(event){
+  if (event.target.closest('.menu_button')){
+    menuBody.classList.toggle('_active')
+  }
+  if (!event.target.closest('.menu_button')){
+    menuBody.classList.toggle('_active')
+  }
 }
